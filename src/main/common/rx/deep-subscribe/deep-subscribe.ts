16d93725baa67c08ca73630e99eb732f84388586
Nikolay Makhonin <nikolay.makhonin@gmail.com>
2019-04-13 20:50:42 +0300

    deepSubscribe is ready to test

--- src/main/common/rx/deep-subscribe/deep-subscribe.ts
+++ src/main/common/rx/deep-subscribe/deep-subscribe.ts
@@ -1,28 +1,21 @@
 /* tslint:disable */
-import {ArraySet} from '../../lists/ArraySet'
-import {IPropertyChanged} from '../../lists/contracts/IPropertyChanged'
-import {ISetChanged} from '../../lists/contracts/ISetChanged'
-import {IObservable} from '../subjects/observable'
 import {IUnsubscribe} from '../subjects/subject'
 import {IRule} from './contracts/rules'
-import {subscribeChilds} from './helpers/subscribe-childs'
-import {IRuleOrIterable, iterateRule} from './iterate-rule'
-import {IRuleSubscribe} from "./contracts/rule-subscribe";
+import {IRuleOrIterable, iterateRule, subscribeNextRule} from './iterate-rule'
 
-export interface IDeepSubscribeOptions {
-
+export interface IDeepSubscribeOptions<TValue> {
+	bind: (value: TValue) => IUnsubscribe
+	immediate?: boolean
 }
 
 function deepSubscribe<TValue>(
 	object: any,
 	rule: IRule,
-	bind: (value: TValue) => IUnsubscribe,
-	options: IDeepSubscribeOptions,
+	options: IDeepSubscribeOptions<TValue>,
 ): IUnsubscribe {
 	return _deepSubscribe<TValue>(
 		object,
 		iterateRule(rule)[Symbol.iterator](),
-		bind,
 		options,
 	)
 }
@@ -30,125 +23,85 @@ function deepSubscribe<TValue>(
 function *_deepSubscribe<TValue>(
 	object: any,
 	ruleIterator: Iterator<IRuleOrIterable>,
-	bind: (value: TValue) => IUnsubscribe,
-	options: IDeepSubscribeOptions,
+	options: IDeepSubscribeOptions<TValue>,
 	propertiesPath?: string,
 ): any {
-	const iteration = ruleIterator.next()
-	if (iteration.done) {
-		return null
-	}
+	const subscribeNext = () => {
+		let unsubscribePropertyName
 
-	const ruleOrIterable = iteration.value
+		return subscribeNextRule(
+			ruleIterator,
+			nextRuleIterator => _deepSubscribe(object, nextRuleIterator, options, propertiesPath),
+			rule => {
+				function subscribeItem(item, debugPropertyName: string) {
+					const newPropertiesPath = (propertiesPath ? propertiesPath + '.' : '')
+						+ debugPropertyName + '(' + rule.description + ')'
 
-	if (ruleOrIterable[Symbol.iterator]) {
-		const unsubscribers: IUnsubscribe[] = []
-		for (const ruleIterable of ruleOrIterable as Iterable<IRuleOrIterable>) {
-			unsubscribers.push(_deepSubscribe(
-				object,
-				ruleIterable[Symbol.iterator](),
-				bind,
-				options,
-			))
-		}
+					const subscribe = () => _deepSubscribe(
+						item,
+						ruleIterator,
+						options,
+						newPropertiesPath,
+					)
 
-		return () => {
-			for (let i = 0, len = unsubscribers.length; i < len; i++) {
-				unsubscribers[i]()
-			}
-		}
-	} else {
-		const rule = ruleOrIterable as IRuleSubscribe
-		const unsubscribers = new ArraySet<IUnsubscribe>()
+					if (!(item instanceof Object)) {
+						const unsubscribe = subscribe()
+						if (unsubscribe) {
+							throw new Error(`You should not return unsubscribe function (${unsubscribe}) for non Object value (${object}).\nFor subscribe value types use their object wrappers: Number, Boolean, String classes.\nValue property path: ${newPropertiesPath}`)
+						}
+						return
+					}
 
-		function subscribeItem(item, debugPropertyName: string) {
-			let unsubscribe = item.unsubscribe // TODO
-			if (!unsubscribe) {
-				unsubscribe = _deepSubscribe(
-					object,
-					ruleIterator,
-					bind,
-					options,
-					(propertiesPath ? propertiesPath + '.' : '')
-						+ debugPropertyName + '(' + rule.description + ')',
-				)
-				item.unsubscribe = unsubscribe // TODO
-			}
-		}
+					if (!unsubscribePropertyName) {
+						unsubscribePropertyName = Math.random().toString(36)
+					}
 
-		function unsubscribeItem(item, debugPropertyName: string) {
-			const unsubscribe = item.unsubscribe // TODO
-			if (unsubscribe) {
-				unsubscribe()
-				delete item.unsubscribe // TODO
-			}
-		}
+					let unsubscribe = item[unsubscribePropertyName]
+					if (!unsubscribe) {
+						unsubscribe = subscribe()
 
-		{
-			const unsubscribe = subscribeChilds({
-				object,
-				propertyPredicate: rule.predicate,
-				subscribeItem,
-				unsubscribeItem,
-			})
-
-			if (unsubscribe) {
-				unsubscribers.add(unsubscribe)
-			}
-		}
-
-		for (const item of rule.iterateObject(object)) {
-			subscribeItem(item)
-		}
-
-		return () => {
-			for (const item of rule.iterateObject(object)) {
-				unsubscribeItem(item)
-			}
-		}
-	}
-}
-
-function deepSubscribeOld<TValue>(
-	object: any,
-	rule: IRule,
-	bind: (value: TValue) => IUnsubscribe,
-	options: IDeepSubscribeOptions,
-): IUnsubscribe {
-	const ruleNext = nextRule(rule)
-
-	if (object.propertyChanged) {
-		(object as IPropertyChanged)
-			.propertyChanged
-			.subscribe(event => {
-
-				const unsubscribe = deepSubscribe(
-					event.newValue,
-					ruleNext,
-					value => {
-
-					},
-					options)
-			})
-	} else if (object.setChanged) {
-		(object as ISetChanged<any>)
-			.setChanged
-			.subscribe(event => {
-
-				for (const newItem of event.newItems) {
-					const unsubscribe = deepSubscribe(
-						newItem,
-						ruleNext,
-						value => {
-
-						},
-						options)
+						Object.defineProperty(item, unsubscribePropertyName, {
+							configurable: true,
+							enumerable: false,
+							writable: false,
+							value: unsubscribe
+						})
+					}
 				}
-			})
-	} else if (object.mapChanged) {
 
-	} else if (object.listChanged) {
+				function unsubscribeItem(item, debugPropertyName: string) {
+					if (!(item instanceof Object)) {
+						return
+					}
 
+					if (!unsubscribePropertyName) {
+						return
+					}
+
+					const unsubscribe = item[unsubscribePropertyName]
+					if (unsubscribe) {
+						unsubscribe()
+						delete item[unsubscribePropertyName]
+					}
+				}
+
+				return rule.subscribe(
+					object,
+					options.immediate,
+					subscribeItem,
+					unsubscribeItem,
+				)
+			},
+			() => {
+				return options.bind(object)
+			},
+		)
 	}
 
+	try {
+		return subscribeNext()
+	} catch (ex) {
+		ex.message += `\nObject property path: ${propertiesPath}`
+		throw ex
+	}
 }
