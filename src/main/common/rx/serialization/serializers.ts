9a705552b8f7159e17432d2deb2d1f8926c1f114
Nikolay Makhonin <nikolay.makhonin@gmail.com>
2019-04-27 08:01:09 +0300

    Develop

--- src/main/common/rx/serialization/serializers.ts
+++ src/main/common/rx/serialization/serializers.ts
@@ -1,3 +1,5 @@
+import {ITypeMeta} from './TypeMeta'
+
 // import {ISerializable} from './contracts'
 
 // export interface IWriter {
@@ -66,94 +68,6 @@ export interface IValueSerializer {
 		serializedValue: ISerializedTypedValue,
 		valueFactory?: () => TValue,
 	): TValue
-}
-
-// endregion
-
-// region TypeMeta
-
-export type TClass = new () => any
-
-export interface ITypeMeta {
-	uuid: string
-}
-
-export interface ITypeMetaCollection<TMeta extends ITypeMeta> {
-	getType(uuid: string): TClass
-	getMeta(type: TClass): TMeta
-	putType(type: TClass, meta: TMeta): TMeta
-	deleteType(typeOrUuid: TClass|string): TMeta
-}
-
-const typeMetaPropertyNameBase: string = Math.random().toString(36)
-let typeMetaPropertyNameIndex: number = 0
-
-export class TypeMetaCollection<TMeta extends ITypeMeta> implements ITypeMetaCollection<TMeta> {
-	private readonly _typeMetaPropertyName: string = typeMetaPropertyNameBase + (typeMetaPropertyNameIndex++)
-	private readonly _typeMap: { [uuid: string]: TClass }
-	private readonly _proto: ITypeMetaCollection<TMeta>
-
-	constructor(proto: ITypeMetaCollection<TMeta>) {
-		if (proto) {
-			this._proto = proto
-		}
-	}
-
-	public getMeta(type: TClass): TMeta {
-		const meta = type[this._typeMetaPropertyName]
-
-		const {_proto} = this
-		if (_proto && typeof meta === 'undefined') {
-			return _proto.getMeta(type)
-		}
-	}
-
-	public getType(uuid: string): TClass {
-		const type = this._typeMap[uuid]
-
-		const {_proto} = this
-		if (_proto && typeof type === 'undefined') {
-			return _proto.getType(uuid)
-		}
-	}
-
-	public putType(type: TClass, meta: TMeta): TMeta {
-		if (!type || typeof type !== 'function') {
-			throw new Error(`type (${type}) should be function`)
-		}
-
-		const uuid = meta.uuid
-		if (!uuid || typeof uuid !== 'string') {
-			throw new Error(`meta.uuid (${uuid}) should be a string with length > 0`)
-		}
-
-		this._typeMap[uuid] = type
-		const prevMeta = type[this._typeMetaPropertyName]
-		type[this._typeMetaPropertyName] = meta
-
-		return prevMeta
-	}
-
-	public deleteType(typeOrUuid: TClass | string): TMeta {
-		let uuid
-		let type
-		if (typeof typeOrUuid === 'function') {
-			const meta = this.getMeta(typeOrUuid)
-			uuid = meta && meta.uuid
-			type = typeOrUuid
-		} else if (typeof typeOrUuid === 'string') {
-			type = this.getType(typeOrUuid)
-			uuid = typeOrUuid
-		} else {
-			return
-		}
-
-		const prevMeta = type[this._typeMetaPropertyName]
-		delete type[this._typeMetaPropertyName]
-		delete this._typeMap[uuid]
-
-		return prevMeta
-	}
 }
 
 // endregion
