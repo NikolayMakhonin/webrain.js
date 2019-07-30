b54f39ad295ebf4ebda838f1fd07742a80d92183
Nikolay Makhonin <nikolay.makhonin@gmail.com>
2019-07-30 07:18:06 +0300

    Develop

--- src/main/common/extensions/merge/merge-object.ts
+++ src/main/common/extensions/merge/merge-object.ts
@@ -2,7 +2,7 @@
 import {IMergeValue} from './contracts'
 
 // tslint:disable-next-line:no-empty
-const NONE: any = function() {}
+function NONE() {}
 
 export function mergeObject<TObject extends object>(
 	merge: IMergeValue,
@@ -81,4 +81,139 @@ export function mergeObject<TObject extends object>(
 	}
 
 	return changed
+}
+
+interface IMergeMapWrapper<K, V> {
+	has(key: K): boolean
+	get(key: K): V
+	set(key: K, value: V): void
+	delete(key: K): void
+	forEachKeys(callbackfn: (key: K) => void): void
+}
+
+function mergeMapWrappers<K, V>(
+	merge: IMergeValue,
+	base: IMergeMapWrapper<K, V>,
+	older: IMergeMapWrapper<K, V>,
+	newer: IMergeMapWrapper<K, V>,
+	preferCloneOlder?: boolean,
+	preferCloneNewer?: boolean,
+): boolean {
+	let changed = false
+
+	const addItems = []
+
+	if (older === newer) {
+		// [- n n]
+		older.forEachKeys(key => {
+			if (!base.has(key)) {
+				addItems.push([key, older.get(key)])
+			}
+		})
+	} else {
+		// [- - n]
+		newer.forEachKeys(key => {
+			if (!base.has(key) && !older.has(key)) {
+				addItems.push([key, newer.get(key)])
+			}
+		})
+
+		// [- o *]
+		older.forEachKeys(key => {
+			if (!base.has(key)) {
+				if (!newer.has(key)) {
+					addItems.push([key, older.get(key)])
+				} else {
+					let setItem = NONE
+
+					merge(NONE, older.get(key), newer.get(key), o => {
+						setItem = o
+					}, preferCloneOlder, preferCloneOlder)
+
+					if (setItem === NONE) {
+						throw new Error('setItem === NONE')
+					}
+
+					addItems.push([key, setItem])
+				}
+			}
+		})
+	}
+
+	// [b * *]
+	base.forEachKeys(key => {
+		changed = merge(
+			base.get(key),
+			older.has(key) ? older.get(key) : NONE,
+			newer.has(key) ? newer.get(key) : NONE,
+			o => {
+				if ((o as any) === NONE) {
+					base.delete(key)
+					changed = true
+				} else {
+					base.set(key, o)
+				}
+			}, preferCloneOlder, preferCloneNewer) || changed
+	})
+
+	const len = addItems.length
+	if (len > 0) {
+		changed = true
+		for (let i = 0; i < len; i++) {
+			base.set.apply(base, addItems[i])
+		}
+	}
+
+	return changed
+}
+
+export class MergeObjectWrapper implements IMergeMapWrapper<string, any> {
+	private readonly _object: { [key: string]: any }
+
+	constructor(object: { [key: string]: any }) {
+		this._object = object
+	}
+
+	public delete(key: string): void {
+		delete this._object[key]
+	}
+
+	public forEachKeys(callbackfn: (key: string) => void): void {
+		const {_object} = this
+		for (const key in _object) {
+			if (Object.prototype.hasOwnProperty.call(_object, key)) {
+				callbackfn(key)
+			}
+		}
+	}
+
+	public get(key: string): any {
+		return this._object[key]
+	}
+
+	public has(key: string): boolean {
+		return Object.prototype.hasOwnProperty.call(this._object, key)
+	}
+
+	public set(key: string, value: any): void {
+		this._object[key] = value
+	}
+}
+
+export function mergeObject2<TObject extends object>(
+	merge: IMergeValue,
+	base: TObject,
+	older: TObject,
+	newer: TObject,
+	preferCloneOlder?: boolean,
+	preferCloneNewer?: boolean,
+): boolean {
+	return mergeMapWrappers(
+		merge,
+		new MergeObjectWrapper(base),
+		new MergeObjectWrapper(older),
+		new MergeObjectWrapper(newer),
+		preferCloneOlder,
+		preferCloneNewer,
+	)
 }
--- src/test/performance/common/ObjectMerger.ts
+++ src/test/performance/common/ObjectMerger.ts
@@ -8,25 +8,20 @@ declare const babelGetFileCode
 describe('common > extensions > merge > ObjectMerger', function() {
 	this.timeout(60000)
 
-	it('performance', function() {
-		const values = [
-			null, void 0, 0, 1, false, true, '', '1', {}, { a: {a: 1, b: 2}, b: 3 }, { a: {b: 4, c: 5}, c: 6 }, { a: {a: 7, b: 8}, d: 9 }, Object.freeze({ x: {y: 1} }), new Date(1), new Date(2),
-			null, void 0, 0, 1, false, true, '', '1', {}, { a: {a: 1, b: 2}, b: 3 }, { a: {b: 4, c: 5}, c: 6 }, { a: {a: 7, b: 8}, d: 9 }, Object.freeze({ x: {y: 1} }), new Date(1), new Date(2),
-			null, void 0, 0, 1, false, true, '', '1', {}, { a: {a: 1, b: 2}, b: 3 }, { a: {b: 4, c: 5}, c: 6 }, { a: {a: 7, b: 8}, d: 9 }, Object.freeze({ x: {y: 1} }), new Date(1), new Date(2),
-		]
-		const merger = new ObjectMerger()
+	const merger = new ObjectMerger()
 
-		function getRandomValue() {
-			return values[Math.floor(Math.random() * values.length)]
-		}
+	function getRandomValue(values: any[]) {
+		return values[Math.floor(Math.random() * values.length)]
+	}
 
+	function perfTest(name: string, values: any[]) {
 		let time = new Date().getTime()
 		let count = 0
 		while (true) {
 			merger.merge(
-				getRandomValue(),
-				getRandomValue(),
-				getRandomValue(),
+				getRandomValue(values),
+				getRandomValue(values),
+				getRandomValue(values),
 				o => {},
 				Math.random() > 0.5,
 				Math.random() > 0.5,
@@ -39,6 +34,22 @@ describe('common > extensions > merge > ObjectMerger', function() {
 		}
 
 		time = (new Date().getTime() - time) / 1000
-		console.log(`Count tests: ${count}\r\nOperations per second: ${count / time}`)
+		console.log(`${name} - Count tests: ${count} - Operations per second: ${count / time}\r\n\r\n`)
+	}
+
+	it('all', function() {
+		perfTest('all', [
+			null, void 0, 0, 1, false, true, '', '1', {}, { a: {a: 1, b: 2}, b: 3 }, { a: {b: 4, c: 5}, c: 6 }, { a: {a: 7, b: 8}, d: 9 }, Object.freeze({ x: {y: 1} }), new Date(1), new Date(2),
+			null, void 0, 0, 1, false, true, '', '1', {}, { a: {a: 1, b: 2}, b: 3 }, { a: {b: 4, c: 5}, c: 6 }, { a: {a: 7, b: 8}, d: 9 }, Object.freeze({ x: {y: 1} }), new Date(1), new Date(2),
+			null, void 0, 0, 1, false, true, '', '1', {}, { a: {a: 1, b: 2}, b: 3 }, { a: {b: 4, c: 5}, c: 6 }, { a: {a: 7, b: 8}, d: 9 }, Object.freeze({ x: {y: 1} }), new Date(1), new Date(2),
+		])
+	})
+
+	it('objects', function() {
+		perfTest('objects', [
+			{}, { a: {a: 1, b: 2}, b: 3 }, { a: {b: 4, c: 5}, c: 6 }, { a: {a: 7, b: 8}, d: 9 },
+			{}, { a: {a: 1, b: 2}, b: 3 }, { a: {b: 4, c: 5}, c: 6 }, { a: {a: 7, b: 8}, d: 9 },
+			{}, { a: {a: 1, b: 2}, b: 3 }, { a: {b: 4, c: 5}, c: 6 }, { a: {a: 7, b: 8}, d: 9 },
+		])
 	})
 })
