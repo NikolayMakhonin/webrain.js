6fd96327f2a5bf3ed8866e78fe23fbae4007ad11
Nikolay Makhonin <nikolay.makhonin@gmail.com>
2019-08-21 13:53:57 +0300

    Develop calc property

--- src/main/common/rx/object/properties/calc.ts
+++ src/main/common/rx/object/properties/calc.ts
@@ -1,11 +1,31 @@
+import {isThenable, Thenable, ThenableOrValue} from '../../../async/async'
+import {resolveAsync, resolveAsyncFunc} from '../../../async/ThenableSync'
+import {DeferredCalc, IDeferredCalcOptions} from '../../deferred-calc/DeferredCalc'
 import {ObservableObject} from '../ObservableObject'
+import {ObservableObjectBuilder} from '../ObservableObjectBuilder'
+import {IPropertyOptions, Property} from './property'
 
-export class CalcProperty<TValue> extends ObservableObject {
-	private readonly _calcFunc: () => TValue
+export type CalcPropertyFunc<TInput, TTarget, TSource>
+	= (input: TInput, value: Property<TTarget, TSource>) => ThenableOrValue<void>
+
+const valuePropertiesNames = ['current', 'wait', 'currentOrWait']
+
+export class CalcProperty<TInput, TValue, TMergeSource> extends ObservableObject {
+	private readonly _calcFunc: CalcPropertyFunc<TInput, TValue, TMergeSource>
+	private readonly _value: Property<TValue, TMergeSource>
+	private readonly _deferredCalc: DeferredCalc
+	private _waiter: ThenableOrValue<TValue>
+	private hasValue: boolean
 	private isCalculated: boolean
-	private _value: TValue
 
-	constructor(calcFunc: () => TValue) {
+	public input: TInput
+
+	constructor(
+		calcFunc: CalcPropertyFunc<TInput, TValue, TMergeSource>,
+		calcOptions: IDeferredCalcOptions,
+		valueOptions?: IPropertyOptions<TValue, TMergeSource>,
+		value?: TValue,
+	) {
 		super()
 
 		if (typeof calcFunc !== 'function') {
@@ -13,22 +33,64 @@ export class CalcProperty<TValue> extends ObservableObject {
 		}
 
 		this._calcFunc = calcFunc
+		this._value = new Property(valueOptions, value)
+
+		this._deferredCalc = new DeferredCalc(
+			() => {
+				this.onPropertyChanged(propertyNames)
+			},
+			(done: () => void) => {
+				this._waiter = resolveAsyncFunc(
+					() => this._calcFunc(this.input, this._value),
+					() => {
+						const val = this._value.value
+						done()
+						return val
+					},
+					done,
+					true,
+				)
+			},
+			function() {
+				this.onPropertyChanged(propertyNames)
+			},
+			calcOptions,
+		)
 	}
 
-	get value(): TValue {
-		if (this.isCalculated) {
-			return this._value
+	get current(): TValue {
+		if (!this.isCalculated) {
+			this._deferredCalc.calc()
+
+			if (!isThenable(this._waiter)) {
+				return this._waiter as TValue
+			}
 		}
 
-		const value = this._calcFunc()
-		this._value = value
+		return this._value.value
+	}
 
-		this.isCalculated = true
+	get wait(): ThenableOrValue<TValue> {
+		if (!this.isCalculated) {
+			this._deferredCalc.calc()
+		}
 
-		return value
+		return this._waiter as TValue
+	}
+
+	get currentOrWait(): ThenableOrValue<TValue> {
+		if (!this.isCalculated) {
+			this._deferredCalc.calc()
+			if (!isThenable(this._waiter)) {
+				return this._waiter as TValue
+			}
+		}
+
+		return this._value.value
 	}
 
 	public invalidate(): void {
+		// this.onPropertyChanged(valuePropertiesNames)
 		if (this.isCalculated) {
 			const {_propertyChangedIfCanEmit} = this
 			if (!_propertyChangedIfCanEmit) {
@@ -53,3 +115,6 @@ export class CalcProperty<TValue> extends ObservableObject {
 		}
 	}
 }
+
+new ObservableObjectBuilder(CalcProperty.prototype)
+	.writable('input')
