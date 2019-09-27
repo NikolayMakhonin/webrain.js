6b54f6c4efddfb684043e0e2270eacd652c71627
Nikolay Makhonin <nikolay.makhonin@gmail.com>
2019-09-27 08:36:12 +0300

    Facade changes

--- src/main/common/index.ts
+++ src/main/common/index.ts
@@ -7,6 +7,12 @@ export {connectorFactory} from './rx/object/properties/ConnectorBuilder'
 export {Property} from './rx/object/properties/Property'
 export {createFunction} from './helpers/helpers'
 export {CalcObjectDebugger} from './rx/object/properties/CalcObjectDebugger'
+export {getObjectUniqueId} from './helpers/object-unique-id'
+export {ArraySet} from './lists/ArraySet'
+export {ArrayMap} from './lists/ArrayMap'
+export {ObservableSet} from './lists/ObservableSet'
+export {ObservableMap} from './lists/ObservableMap'
+export {deepSubscribe} from './rx/deep-subscribe/deep-subscribe'
 
 // Interfaces:
 import {ThenableOrIteratorOrValue as _ThenableOrIteratorOrValue} from './async/async'
