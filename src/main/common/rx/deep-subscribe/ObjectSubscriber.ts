34c60db4903b634e00fbde0ff82e5d0b6f1e36c3
Nikolay Makhonin <nikolay.makhonin@gmail.com>
2019-09-28 09:09:00 +0300

    ObjectSubscriber refactoring. Tests OK.

--- src/main/common/rx/deep-subscribe/ObjectSubscriber.ts
+++ src/main/common/rx/deep-subscribe/ObjectSubscriber.ts
@@ -9,7 +9,8 @@ import {
 	ISubscribeValue,
 	IUnsubscribeValue,
 	IValueSubscriber,
-	ValueChangeType, ValueKeyType,
+	ValueChangeType,
+	ValueKeyType,
 } from './contracts/common'
 
 interface ISubscribedValue {
@@ -139,206 +140,108 @@ export class ObjectSubscriber<TObject> implements IValueSubscriber<TObject> {
 		ruleDescription: string,
 	): IUnsubscribeOrVoid {
 		if ((changeType & ValueChangeType.Unsubscribe) !== 0) {
-			this.unsubscribe(oldItem, parent, key)
+			if (this._subscribe) {
+				let unsubscribed
+				let unsubscribedLast
+				if (oldItem instanceof Object) {
+					const {_unsubscribersCount} = this
+					if (_unsubscribersCount) {
+						const itemUniqueId = getObjectUniqueId(oldItem as any)
+						const unsubscribeCount = _unsubscribersCount[itemUniqueId]
+						if (unsubscribeCount != null) {
+							if (unsubscribeCount) {
+								if (unsubscribeCount > 1) {
+									_unsubscribersCount[itemUniqueId] = unsubscribeCount - 1
+								} else {
+									const {_unsubscribers} = this
+									const unsubscribe = _unsubscribers[itemUniqueId]
+									// unsubscribers[itemUniqueId] = null // faster but there is a danger of memory overflow with nulls
+									delete _unsubscribers[itemUniqueId]
+									delete _unsubscribersCount[itemUniqueId]
+
+									if (Array.isArray(unsubscribe)) {
+										for (let i = 0, len = unsubscribe.length; i < len; i++) {
+											unsubscribe[i]()
+										}
+									} else {
+										unsubscribe()
+									}
+
+									unsubscribedLast = true
+								}
+							}
+							unsubscribed = true
+						}
+					}
+				}
+
+				if (this._unsubscribe && (unsubscribedLast || !unsubscribed)) {
+					this._unsubscribe(oldItem, parent, key, unsubscribedLast)
+				}
+			}
 		}
 
 		if ((changeType & ValueChangeType.Subscribe) !== 0) {
-			return this.subscribe(newItem, parent, key, propertiesPath, ruleDescription)
-		}
-	}
-
-	private subscribe(
-		value: TObject,
-		parent: any,
-		propertyName: string,
-		propertiesPath: () => string,
-		ruleDescription: string,
-	): IUnsubscribe {
-		if (this._subscribe) { // && typeof value !== 'undefined') {
-			if (!(value instanceof Object)) {
-				const unsubscribeValue = checkIsFuncOrNull(this._subscribe(value, parent, propertyName))
-				if (unsubscribeValue) {
-					unsubscribeValue()
-
-					throw new Error('You should not return unsubscribe function for non Object value.\n'
-						+ 'For subscribe value types use their object wrappers: Number, Boolean, String classes.\n'
-						+ `Unsubscribe function: ${unsubscribeValue}\nValue: ${value}\n`
-						+ `Value property path: ${(propertiesPath ? propertiesPath() + '.' : '')
-						+ (propertyName == null ? '' : propertyName + '(' + ruleDescription + ')')}`)
-				}
-			} else {
-				const itemUniqueId = getObjectUniqueId(value as any)
-
-				let {_unsubscribers, _unsubscribersCount} = this
-				if (_unsubscribers && _unsubscribers[itemUniqueId]) {
-					_unsubscribersCount[itemUniqueId]++
-				} else {
-					if (!_unsubscribers) {
-						this._unsubscribers = _unsubscribers = []
-						this._unsubscribersCount = _unsubscribersCount = []
-					}
-
-					const unsubscribeValue = checkIsFuncOrNull(this._subscribe(value, parent, propertyName))
+			if (this._subscribe) { // && typeof value !== 'undefined') {
+				if (!(newItem instanceof Object)) {
+					const unsubscribeValue = checkIsFuncOrNull(this._subscribe(newItem, parent, key))
 					if (unsubscribeValue) {
-						_unsubscribers[itemUniqueId] = unsubscribeValue
-						_unsubscribersCount[itemUniqueId] = 1
-						// return this._setUnsubscribeObject(itemUniqueId, unsubscribeValue)
+						unsubscribeValue()
+
+						throw new Error('You should not return unsubscribe function for non Object value.\n'
+							+ 'For subscribe value types use their object wrappers: Number, Boolean, String classes.\n'
+							+ `Unsubscribe function: ${unsubscribeValue}\nValue: ${newItem}\n`
+							+ `Value property path: ${(propertiesPath ? propertiesPath() + '.' : '')
+							+ (key == null ? '' : key + '(' + ruleDescription + ')')}`)
 					}
-				}
-			}
-		}
+				} else {
+					const itemUniqueId = getObjectUniqueId(newItem as any)
 
-		if (this._lastValue) {
-			this.insertSubscribed({value, parent, propertyName, isOwnProperty: parent != null && propertyName in parent})
-			// let {_subscribedValues, _subscribedParents, _subscribedPropertyNames} = this
-			// if (!_subscribedValues) {
-			// 	this._subscribedValues = _subscribedValues = []
-			// 	this._subscribedParents = _subscribedParents = []
-			// 	this._subscribedPropertyNames = _subscribedPropertyNames = []
-			// }
-			// let index
-			// const len = _subscribedValues.length
-			// for (let i = len - 1; i >= 0; i--) {
-			// 	if (_subscribedValues[i] === value
-			// 		&& _subscribedParents[i] === parent
-			// 		&& _subscribedPropertyNames[i] === propertyName
-			// 	) {
-			// 		index = i
-			// 		break
-			// 	}
-			// }
-			//
-			// if (index >= 0) {
-			// 	if (index < len - 1) {
-			// 		for (let i = len - 1; i > index; i--) {
-			// 			_subscribedValues[i + 1] = _subscribedValues[i]
-			// 			_subscribedParents[i + 1] = _subscribedParents[i]
-			// 			_subscribedPropertyNames[i + 1] = _subscribedPropertyNames[i]
-			// 		}
-			// 		_subscribedValues[index + 1] = value
-			// 		_subscribedParents[index + 1] = parent
-			// 		_subscribedPropertyNames[index + 1] = propertyName
-			// 	} else {
-			// 		_subscribedValues.push(value)
-			// 		_subscribedParents.push(parent)
-			// 		_subscribedPropertyNames.push(propertyName)
-			// 	}
-			// } else {
-			// 	if (len > 0 && (typeof value === 'undefined' || typeof _subscribedValues[len - 1] !== 'undefined')) {
-			// 		if (typeof value === 'undefined') {
-			// 			_subscribedValues.unshift(value)
-			// 			_subscribedParents.unshift(parent)
-			// 			_subscribedPropertyNames.unshift(propertyName)
-			// 		} else {
-			// 			_subscribedValues[len] = _subscribedValues[len - 1]
-			// 			_subscribedParents[len] = _subscribedParents[len - 1]
-			// 			_subscribedPropertyNames[len] = _subscribedPropertyNames[len - 1]
-			// 			_subscribedValues[len - 1] = value
-			// 			_subscribedParents[len - 1] = parent
-			// 			_subscribedPropertyNames[len - 1] = propertyName
-			// 		}
-			// 	} else {
-			// 		_subscribedValues.push(value)
-			// 		_subscribedParents.push(parent)
-			// 		_subscribedPropertyNames.push(propertyName)
-			// 		this._lastValue(value, parent, propertyName)
-			// 	}
-			// }
-		}
-
-		return () => { this.unsubscribe(value, parent, propertyName) }
-	}
-
-	private unsubscribe(value: TObject, parent: any, propertyName: string) {
-		if (this._subscribe) { //  && typeof value !== 'undefined') {
-			let unsubscribed
-			let unsubscribedLast
-			if (value instanceof Object) {
-				const {_unsubscribersCount} = this
-				if (_unsubscribersCount) {
-					const itemUniqueId = getObjectUniqueId(value as any)
-					const unsubscribeCount = _unsubscribersCount[itemUniqueId]
-					if (unsubscribeCount != null) {
-						if (unsubscribeCount) {
-							if (unsubscribeCount > 1) {
-								_unsubscribersCount[itemUniqueId] = unsubscribeCount - 1
-							} else {
-								const {_unsubscribers} = this
-								const unsubscribe = _unsubscribers[itemUniqueId]
-								// unsubscribers[itemUniqueId] = null // faster but there is a danger of memory overflow with nulls
-								delete _unsubscribers[itemUniqueId]
-								delete _unsubscribersCount[itemUniqueId]
-
-								if (Array.isArray(unsubscribe)) {
-									for (let i = 0, len = unsubscribe.length; i < len; i++) {
-										unsubscribe[i]()
-									}
-								} else {
-									unsubscribe()
-								}
-
-								unsubscribedLast = true
-							}
+					let {_unsubscribers, _unsubscribersCount} = this
+					if (_unsubscribers && _unsubscribers[itemUniqueId]) {
+						_unsubscribersCount[itemUniqueId]++
+					} else {
+						if (!_unsubscribers) {
+							this._unsubscribers = _unsubscribers = []
+							this._unsubscribersCount = _unsubscribersCount = []
 						}
-						unsubscribed = true
+
+						const unsubscribeValue = checkIsFuncOrNull(this._subscribe(newItem, parent, key))
+						if (unsubscribeValue) {
+							_unsubscribers[itemUniqueId] = unsubscribeValue
+							_unsubscribersCount[itemUniqueId] = 1
+							// return this._setUnsubscribeObject(itemUniqueId, unsubscribeValue)
+						}
 					}
 				}
 			}
+		}
 
-			if (this._unsubscribe && (unsubscribedLast || !unsubscribed)) {
-				this._unsubscribe(value, parent, propertyName, unsubscribedLast)
+		if ((changeType & ValueChangeType.Unsubscribe) !== 0) {
+			if (this._lastValue) {
+				this.removeSubscribed({value: oldItem, parent, propertyName: key})
 			}
 		}
 
-		if (this._lastValue) {
-			this.removeSubscribed({value, parent, propertyName})
-			// const {_subscribedValues, _subscribedParents, _subscribedPropertyNames} = this
-			// if (_subscribedValues) {
-			// 	let index = -1
-			// 	const len = _subscribedValues.length
-			// 	for (let i = 0; i < len; i++) {
-			// 		if (_subscribedValues[i] === value
-			// 			&& _subscribedParents[i] === parent
-			// 			&& _subscribedPropertyNames[i] === propertyName
-			// 		) {
-			// 			index = i
-			// 			break
-			// 		}
-			// 	}
-			//
-			// 	if (index < 0) {
-			// 		if (typeof value !== 'undefined') {
-			// 			throw new Error(`subscribedValue no found: ${parent.constructor.name}.${propertyName} = ${value}`)
-			// 		}
-			// 	} else {
-			// 		if (index === len - 1) {
-			// 			_subscribedValues.length = len - 1
-			// 			_subscribedParents.length = len - 1
-			// 			_subscribedPropertyNames.length = len - 1
-			// 			if (len > 1) {
-			// 				this._lastValue(
-			// 					_subscribedValues[len - 2],
-			// 					_subscribedParents[len - 2],
-			// 					_subscribedPropertyNames[len - 2],
-			// 				)
-			// 			} else {
-			// 				this._lastValue(void 0, null, null)
-			// 			}
-			// 		} else {
-			// 			if (index !== len - 2) {
-			// 				_subscribedValues[index] = _subscribedValues[len - 2]
-			// 				_subscribedParents[index] = _subscribedParents[len - 2]
-			// 				_subscribedPropertyNames[index] = _subscribedPropertyNames[len - 2]
-			// 			}
-			// 			_subscribedValues[len - 2] = _subscribedValues[len - 1]
-			// 			_subscribedParents[len - 2] = _subscribedParents[len - 1]
-			// 			_subscribedPropertyNames[len - 2] = _subscribedPropertyNames[len - 1]
-			// 			_subscribedValues.length = len - 1
-			// 			_subscribedParents.length = len - 1
-			// 			_subscribedPropertyNames.length = len - 1
-			// 		}
-			// 	}
-			// }
+		if ((changeType & ValueChangeType.Subscribe) !== 0) {
+			if (this._lastValue) {
+				this.insertSubscribed({value: newItem, parent, propertyName: key, isOwnProperty: parent != null && key in parent})
+			}
+		}
+
+		if ((changeType & ValueChangeType.Subscribe) !== 0) {
+			return () => {
+				this.change(
+					key,
+					newItem,
+					void 0,
+					parent,
+					ValueChangeType.Unsubscribe,
+					keyType,
+					propertiesPath,
+					ruleDescription,
+				)
+			}
 		}
 	}
 }
