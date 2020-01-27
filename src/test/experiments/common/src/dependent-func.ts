31132785ecd669e1b47c390e27250924010bb2b1
Nikolay Makhonin <nikolay.makhonin@gmail.com>
2020-01-27 17:14:22 +0200

    Develop an automatic subscription system using a brilliant idea from MobX, but more universal

--- src/test/experiments/common/src/dependent-func.ts
+++ src/test/experiments/common/src/dependent-func.ts
@@ -1,71 +1,43 @@
-import {assert} from '../../../../main/common'
-import {isThenable, Thenable, ThenableOrValue} from '../../../../main/common/async/async'
-import {resolveAsync} from '../../../../main/common/async/ThenableSync'
 import {isIterator} from '../../../../main/common/helpers/helpers'
-import {getTupleId, getTupleIdMap} from '../../../../main/common/helpers/tuple-unique-id'
-import {Diff} from '../../../../main/common/helpers/typescript'
-import {IChangeItem} from '../../../../main/common/rx/deep-subscribe/contracts/rule-subscribe'
-import {IUnsubscribeOrVoid} from '../../../../main/common/rx/subjects/observable'
-import {ISubject, Subject} from '../../../../main/common/rx/subjects/subject'
 
-type NotThenable<T> = Diff<T, Thenable<any>>
-type TDependentFuncBodyResult<T> = Iterator<NotThenable<T>> | NotThenable<T>
-type TDependentFuncBody<T> = () => TDependentFuncBodyResult<T>
+export type Func<TThis, TArgs extends any[], TResult = void> = (this: TThis, ...args: TArgs) => TResult
 
-type TDependentFunc<T> = () => ThenableOrValue<T>
-interface IDependentFunc<T> extends TDependentFunc<T> {
-	name: string
-	subscribe(subscribe: (changeItem: IChangeItem<any>) => IUnsubscribeOrVoid): void
-	changed: ISubject<T>
+export type TDelegate<
+	TThis,
+	TArgs extends any[],
+	TFuncResult = void,
+	TDelegateResult = void,
+> = (func: Func<TThis, TArgs, TFuncResult>) => Func<TThis, TArgs, TDelegateResult>
+
+export enum FuncStatus {
+	Calculating = 'Calculating',
+	Calculated = 'Calculated',
 }
 
-// const acceptorSubscribe: IAcceptorSubscribe = subscribe => {
-// 	const unsubscriber = subscribe((
-// 		key: any,
-// 		oldItem: any,
-// 		newItem: any,
-// 		changeType: ValueChangeType,
-// 		keyType: ValueKeyType,
-// 	) => {
-// 		invalidate()
-// 	})
-//
-// 	if (this.unsubscribers) {
-// 		this.unsubscribers = [unsubscriber]
-// 	} else {
-// 		this.unsubscribers.push(unsubscriber)
-// 	}
-// }
+type TChangeState<TValue> = (status: FuncStatus, value?: TValue, error?: any) => void
 
-// currentPushSubscribeDependency(changeItem => {
-// 	o.propertyChanged.subscribe((...args) => {
-// 		changeItem(...)
-// 	})
-//
-// 	changeItem(...)
-//
-// 	return () => {
-// 		changeItem(...)
-// 	}
-// })
-
-// terms: dependent <- dependency
-
-export type ISubscribeDependency = (onInvalidate: () => void) => IUnsubscribeOrVoid
-
-export interface IFuncMeta {
-	hasDependency(callId: number): boolean
-	subscribeDependency(callId: number, subscriber: ISubscribeDependency)
-	setValue(callId: number, value: any)
+function changeState<
+	TThis,
+	TArgs extends any[],
+	TResult,
+>(func: Func<TThis, TArgs, TResult>): Func<TThis, TArgs, TChangeState<TResult>> {
+	return null // TODO
 }
 
-let currentMeta: IFuncMeta
+type TMeta = TDelegate<any, any, any>
 
-function* makeDependentIterator<T>(
-	callId: number,
-	meta: IFuncMeta,
-	iterator: Iterator<T>,
-): Iterator<T> {
+let currentMeta: TMeta
+
+function* makeDependentIterator<
+	TThis,
+	TArgs extends any[],
+	TResult,
+	TFunc extends Func<TThis, TArgs, TResult>
+>(
+	meta: TMeta,
+	_changeState: TChangeState<TResult>,
+	iterator: Iterator<TResult>,
+): Iterator<TResult> {
 	currentMeta = meta
 
 	let iteration = iterator.next()
@@ -75,7 +47,7 @@ function* makeDependentIterator<T>(
 		iteration = iterator.next(value)
 	}
 
-	meta.setValue(callId, iteration.value)
+	_changeState(FuncStatus.Calculated, iteration.value)
 
 	return iteration.value
 }
@@ -84,65 +56,40 @@ export function makeDependentFunc<
 	TThis,
 	TArgs extends any[],
 	TResult,
-	TFunc extends (this: TThis, ...args: TArgs) => TResult
+	TFunc extends Func<TThis, TArgs, TResult>
 >(
 	func: TFunc,
-	getSubscriber: (this: TThis, ...args: TArgs) => ISubscribeDependency,
-	meta: IFuncMeta,
+	meta: TMeta,
 ): TFunc {
-	const callIdMap = getTupleIdMap(func)
-
 	return function() {
+		let _changeState
+
 		const parentMeta = currentMeta
-		currentMeta = meta
 
-		const callId = getTupleId.apply(
-			callIdMap.getTupleIdMap(arguments.length, this),
-			arguments,
-		)
-
-		if (parentMeta && !parentMeta.hasDependency(callId)) {
-			parentMeta.subscribeDependency(callId, getSubscriber.apply(this, arguments))
-		}
-
-		let result
 		try {
-			result = func.apply(this, arguments)
-			if (isIterator(result)) {
-				return makeDependentIterator(callId, meta, result as Iterator<any>)
+			currentMeta = meta
+
+			if (parentMeta) {
+				parentMeta(func).apply(this, arguments)
 			}
-			meta.setValue(callId, result)
+
+			_changeState = changeState(func).apply(this, arguments)
+
+			_changeState(FuncStatus.Calculating)
+
+			const result = func.apply(this, arguments)
+
+			if (isIterator(result)) {
+				return makeDependentIterator(meta, _changeState, result as Iterator<any>)
+			} else {
+				_changeState(FuncStatus.Calculated, result)
+			}
+
 			return result
-		} finally {
+		} catch (error) {
 			currentMeta = parentMeta
+			_changeState(FuncStatus.Calculated, void 0, error)
 		}
+
 	} as any
-}
-
-class DependentFunc<
-	TThis,
-	TArgs extends any[],
-	TResult,
-	TFunc extends (this: TThis, ...args: TArgs) => TResult
-> {
-	private readonly _func: TFunc
-	constructor(func: TFunc) {
-		this.id = id
-	}
-
-	public subscribers = new Map()
-	public values = new Map()
-
-	public hasDependency(callId: number): boolean {
-		return this.subscribers.has(callId)
-	}
-
-	public subscribeDependency(callId: number, subscriber: (onInvalidate: () => void) => IUnsubscribeOrVoid) {
-		assert.notOk(this.subscribers.has(callId))
-		return this.subscribers.set(callId, subscriber)
-	}
-
-	public setValue(callId: number, value: any) {
-		return this.values.set(callId, value)
-	}
 }
