7c4901873e5e932b4af6f87984cd263ab4f83af7
Nikolay Makhonin <nikolay.makhonin@gmail.com>
2020-02-07 09:24:30 +0200

    Develop dependentFunc. Before refactoring
    perceptron 100 x 10
    invalidate cycles: 12089
    calc cycles: 159433
    create bytes: 641.5022072204638 ±3062.0643185018303
    recalc bytes: 15883.9371154598 ±10539.072818593493

--- src/test/performance/node/dependent-func.ts
+++ src/test/performance/node/dependent-func.ts
@@ -6,7 +6,7 @@ import {describe, it, xit} from '../../../main/common/test/Mocha'
 import {createPerceptron} from '../../tests/common/main/rx/depend/src/helpers'
 
 describe('dependent-func', function() {
-	xit('perceptron perf', function() {
+	it('perceptron perf', function() {
 		this.timeout(300000)
 
 		const {
@@ -14,7 +14,7 @@ describe('dependent-func', function() {
 			input,
 			inputState,
 			output,
-		} = createPerceptron(100, 100)
+		} = createPerceptron(100, 10)
 
 		const result = calcPerformance(
 			10000,
@@ -31,8 +31,9 @@ describe('dependent-func', function() {
 		const cyclesPerSecond = result.calcInfo.iterationCycles * result.calcInfo.iterations / result.calcInfo.testTime * 1000
 		console.log('cyclesPerSecond: ' + cyclesPerSecond)
 		console.log('countFuncs: ' + countFuncs)
-		console.log(`absoluteDiff per second: [${result.absoluteDiff.map(o => cyclesPerSecond / o).join(', ')}]`)
-		console.log(`absoluteDiff per frame: [${result.absoluteDiff.map(o => cyclesPerSecond / o / 60).join(', ')}]`)
+		console.log(`absoluteDiff per func: [${result.absoluteDiff.map(o => o / countFuncs).join(', ')}]`)
+		console.log(`absoluteDiff per second: [${result.absoluteDiff.map(o => countFuncs * cyclesPerSecond / o).join(', ')}]`)
+		console.log(`absoluteDiff per frame: [${result.absoluteDiff.map(o => countFuncs * cyclesPerSecond / o / 60).join(', ')}]`)
 	})
 
 	it('perceptron memory create', function() {
@@ -40,8 +41,8 @@ describe('dependent-func', function() {
 
 		let countFuncs
 
-		console.log(calcMemAllocate(CalcType.Stat, 100, () => {
-			countFuncs = createPerceptron(100, 100, false).countFuncs
+		console.log(calcMemAllocate(CalcType.Stat, 1000, () => {
+			countFuncs = createPerceptron(100, 10, false).countFuncs
 		}).scale(1 / countFuncs).toString())
 	})
 
