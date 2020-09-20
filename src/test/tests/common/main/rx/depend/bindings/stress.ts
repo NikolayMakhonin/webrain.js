c81cae891845d9bdd95a2cc1251f3f5195d86632
Nikolay Makhonin <nikolay.makhonin@gmail.com>
2020-09-20 14:43:31 +0300

    Develop two way binding tests...

--- src/test/tests/common/main/rx/depend/bindings/stress.ts
+++ src/test/tests/common/main/rx/depend/bindings/stress.ts
@@ -2,7 +2,10 @@
 import {AsyncValueOf} from '../../../../../../../main/common/async/async'
 import {webrainOptions} from '../../../../../../../main/common/helpers/webrainOptions'
 import {Random} from '../../../../../../../main/common/random/Random'
-import {describe, it, xdescribe, xit} from '../../../../../../../main/common/test/Mocha'
+import {ObservableClass} from '../../../../../../../main/common/rx/object/ObservableClass'
+import {ObservableObjectBuilder} from '../../../../../../../main/common/rx/object/ObservableObjectBuilder'
+import {assert} from '../../../../../../../main/common/test/Assert'
+import {describe, it} from '../../../../../../../main/common/test/Mocha'
 import {
 	ISearchBestErrorMetrics,
 	randomTestBuilder,
@@ -23,50 +26,208 @@ describe('common > main > rx > depend > bindings > stress', function() {
 		webrainOptions.callState.garbageCollect.minLifeTime = 500
 	})
 
+	interface IObject {
+		prop1?: number
+		prop2?: number
+		prop3?: number
+	}
+
+	class ObjectClass
+		extends ObservableClass
+		implements IObject
+	{
+		public prop1: number
+		public prop2: number
+		public prop3: number
+	}
+
+	new ObservableObjectBuilder(ObjectClass.prototype)
+		.writable('prop1')
+		.writable('prop2')
+		.writable('prop3')
+
+	function fillObject(rnd: Random, obj: IObject) {
+		obj.prop1 = rnd.nextInt(1000)
+		obj.prop2 = rnd.nextInt(1000)
+		obj.prop3 = rnd.nextInt(1000)
+		return obj
+	}
+
+	function generateObject(rnd: Random) {
+		return fillObject(rnd, new ObjectClass())
+	}
+
+	function generateCheckObject(rnd: Random) {
+		return fillObject(rnd, { })
+	}
+
+	function generateItems<TItem>(rnd: Random, count: number, generateItem: (rnd: Random) => TItem) {
+		const items = []
+		for (let i = 0; i < count; i++) {
+			items.push(generateItem(rnd))
+		}
+		return items
+	}
+
+	function generateNumber(rnd: Random, pattern: number | number[]) {
+		return Array.isArray(pattern)
+			? rnd.nextInt(pattern[0], pattern[1] + 1)
+			: pattern
+	}
+
+	abstract class ObjectsBase<TObject extends IObject> {
+		public readonly objects: TObject[]
+		public constructor(objects: TObject[]) {
+			this.objects = objects
+		}
+
+		public abstract setValue(objectNumber: number, propName: string, value: number)
+	}
+
+	class Objects extends ObjectsBase<ObjectClass> {
+		public setValue(objectNumber: number, propName: string, value: number) {
+			this.objects[objectNumber][propName] = value
+		}
+	}
+
+	class CheckObjects extends ObjectsBase<IObject> {
+		private onChange(objectNumber: number, propName: string, value: number) {
+			// TODO
+		}
+
+		public setValue(objectNumber: number, propName: string, value: number) {
+			if (this.objects[objectNumber][propName] !== value) {
+				this.objects[objectNumber][propName] = value
+				this.onChange(objectNumber, propName, value)
+			}
+		}
+	}
+
+	function simplifyObject(obj: IObject) {
+		if (obj.constructor === Object) {
+			return obj
+		}
+		const simplify = {}
+		for (let i = 0; i < propNames.length; i++) {
+			const propName = propNames[i]
+			simplify[propName] = obj[propName]
+		}
+		return simplify
+	}
+
+	function simplifyObjects(items: IObject[]) {
+		return items.map(simplifyObject)
+	}
+
+	function assertObjects(actual: IObject[], expected: IObject[]) {
+		assert.deepStrictEqual(simplifyObjects(actual), simplifyObjects(expected))
+	}
+
+	// region metrics
+
 	function createMetrics(testRunnerMetrics: ISearchBestErrorMetrics) {
 		return {
-			count: 0,
+			metric: 0,
 		}
 	}
 	type IMetrics = AsyncValueOf<ReturnType<typeof createMetrics>>
 
+	function compareMetrics(metrics: IMetrics, metricsMin: IMetrics): number {
+		if (metrics.metric !== metricsMin.metric) {
+			return metrics.metric < metricsMin.metric ? -1 : 0
+		}
+		return 0
+	}
+
+	// endregion
+
+	// region options
+
 	function optionsPatternBuilder(metrics: IMetrics, metricsMin: IMetrics) {
 		return {
+			countObjects: [1, 2],
+			countValues: [1, 5],
 			metrics,
 			metricsMin,
 		}
 	}
 	type IOptionsPattern = AsyncValueOf<ReturnType<typeof optionsPatternBuilder>>
 
-	function optionsGenerator(rnd: Random, {
-		metrics,
-		metricsMin,
-	}: IOptionsPattern) {
+	function optionsGenerator(rnd: Random, options: IOptionsPattern) {
 		return {
-			metrics,
-			metricsMin,
+			countObjects: generateNumber(rnd, options.countObjects),
+			countValues: generateNumber(rnd, options.countValues),
+			metrics: options.metrics,
+			metricsMin: options.metricsMin,
 		}
 	}
 	type IOptions = AsyncValueOf<ReturnType<typeof optionsGenerator>>
 
-	function compareMetrics(metrics: IMetrics, metricsMin: IMetrics) {
-		if (metrics.count !== metricsMin.count) {
-			return metrics.count < metricsMin.count
-		}
-		return true
-	}
+	// endregion
+
+	// region state
 
 	function createState(rnd: Random, options: IOptions) {
+		const seed = rnd.nextSeed()
+		const objects = generateItems(new Random(seed), options.countObjects, generateObject)
+		const checkObjects = generateItems(new Random(seed), options.countObjects, generateCheckObject)
+		assertObjects(objects, checkObjects)
+
 		return {
+			objects: new Objects(objects),
+			checkObjects: new CheckObjects(checkObjects),
 			options,
-			x: 'qwe',
 		}
 	}
 	type IState = AsyncValueOf<ReturnType<typeof createState>>
 
+	// endregion
+
+	// region action
+
+	const propNames = ['prop1', 'prop2', 'prop3']
+
 	function action(rnd: Random, state: IState) {
-		// TODO
+		const objectNumber = rnd.nextInt(state.objects.objects.length)
+		const propName = rnd.nextArrayItem(propNames)
+		const value = rnd.nextInt(state.options.countValues)
+
+		state.objects.setValue(objectNumber, propName, value)
+		state.checkObjects.setValue(objectNumber, propName, value)
+
+		assertObjects(state.objects.objects, state.checkObjects.objects)
+
+		// assert.ok(['state_value1', 'state_value2'].indexOf(state.value) >= 0)
+		// assert.ok(['value1', 'value2'].indexOf(state.options.option) >= 0)
+		// assert.ok(typeof state.options.metrics.metric === 'number')
 	}
+
+	// endregion
+
+	// region testIteration
+
+	const testIteration = testIterationBuilder({
+		action: {
+			weight: 1,
+			func: action,
+		},
+	})
+
+	// endregion
+
+	// region testIterator
+
+	const testIterator = testIteratorBuilder(
+		createState,
+		{
+			stopPredicate(iterationNumber, timeStart, state) {
+				return iterationNumber >= 100
+			},
+			testIteration,
+		},
+	)
+
+	// endregion
 
 	// region randomTest
 
@@ -86,20 +247,7 @@ describe('common > main > rx > depend > bindings > stress', function() {
 			searchBestError: searchBestErrorBuilder({
 				consoleOnlyBestErrors: true,
 			}),
-			testIterator: testIteratorBuilder(
-				createState,
-				{
-					stopPredicate(iterationNumber, timeStart, state) {
-						return iterationNumber >= 100
-					},
-					iteration: testIterationBuilder({
-						action: {
-							weight: 1,
-							func: action,
-						},
-					}),
-				},
-			),
+			testIterator,
 		},
 	)
 
@@ -112,7 +260,7 @@ describe('common > main > rx > depend > bindings > stress', function() {
 			},
 			customSeed: null,
 			metricsMin: null,
-			searchBestError: true,
+			searchBestError: false,
 		})
 	})
 })
